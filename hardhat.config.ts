e93e2c89e95a6d7af5dafa29dbf6a7655b4d6c55
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-08-01 22:10:00 +0530

    chore: 🔧 add arbitrum goerli config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: '0.8.19',
+  networks: {
+    'arbitrum-goerli': {
+      url: `https://arb-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      chainId: 421613,
+      accounts: {
+        count: 10,
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/60'/0'/0",
+      },
+    },
+  },
 };
 
 export default config;
